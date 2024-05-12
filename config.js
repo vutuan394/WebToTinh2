cf74c935203c6afb63fc31a14111ca88f48457f0
vutuan394 <138289477+vutuan394@users.noreply.github.com>
2024-05-12 23:40:51 +0700

    1

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.facebook.com/profile.php?id=100084385638106&mibextid=ZbWKwL' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.facebook.com/profile.php?id=100010438634060&mibextid=LQQJ4d' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
